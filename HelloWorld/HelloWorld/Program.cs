8f22ebaa1b518a7421401c85aa719cea964c2289
Charlie Tekulve <charlietekulve@gmail.com>
2022-10-24 01:47:18 -0400

    Big daddy commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Title adds title to console app window
+Console.Title = "Charlie's First App!";
+
+// This is like setting the "marker color" ofor the console. 
+// Any text that we print out after this will be in the color we set.
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+// If we clear after setting the BG color, the window will be 
+// covered in that color.
+Console.Clear();
+
+// Advanced: verbatim string.
+// Allows us to print multi-line text!
+// Font from text to ascii art generator (www.patorjk.com/software/taag)
+
+Console.WriteLine(@" 
+
+                                              ___                                                   
+                                             (   )      .-.                                         
+    .--.     ___  ___  ___   .--.     .--.    | |_     ( __)   .--.        .-..     .--.     .--.   
+  /  _  \   (   )(   )(   ) /    \   /    \  (   __)   (''"")  /    \      /    \   /    \   /    \  
+ . .' `. ;   | |  | |  | | |  .-. ; |  .-. ;  | |       | |  |  .-. ;    ' .-,  ; |  .-. ; |  .-. ; 
+ | '   | |   | |  | |  | | |  | | | |  | | |  | | ___   | |  |  | | |    | |  . | | |  | | | |  | | 
+ _\_`.(___)  | |  | |  | | |  |/  | |  |/  |  | |(   )  | |  |  |/  |    | |  | | | |  | | | |  | | 
+(   ). '.    | |  | |  | | |  ' _.' |  ' _.'  | | | |   | |  |  ' _.'    | |  | | | |  | | | |  | | 
+ | |  `\ |   | |  ; '  | | |  .'.-. |  .'.-.  | ' | |   | |  |  .'.-.    | |  ' | | '  | | | '  | | 
+ ; '._,' '   ' `-'   `-' ' '  `-' / '  `-' /  ' `-' ;   | |  '  `-' /    | `-'  ' '  `-' / '  `-' / 
+  '.___.'     '.__.'.__.'   `.__.'   `.__.'    `.__.   (___)  `.__.'     | \__.'   `.__.'   `.__.'  
+                                                                         | |                        
+                                                                        (___)                       
+
+
+");
+
+
+// WriteLine prints out a line of text to the console
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("HELLO");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write(" there!");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("This is Charlie!");
+
+Console.WriteLine("Press any key to exit...");
+
+// ReadKey waits for a key to be pressed. We use this here
+// to keep the console window open until the user presses a key.
+Console.ReadKey();
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Variables can be useful for info that you need in multiple places:
+        string title = "=== Variables Demo ===";
+        Console.Title = title;
+        Console.WriteLine(title);
+
+        // Variables enable us to work with user input:
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        // The following ways to concatenate are equivalent:
+        Console.WriteLine("Welcome to my silly app, " + name + ".");
+        Console.WriteLine($"Welcome to my silly app, {name}.");
+
+        int playerScore = 0;
+        // We can concatenated a string and a number together
+        Console.WriteLine("Your score is: " + playerScore);
+        // We can also do math:
+        Console.WriteLine(playerScore + 1);
+
+        double itemCost = 10.25;
+        Console.WriteLine("The item's cost is: " + itemCost);
+
+        // Don't do this - type mismatch.
+        //int badInt = 44.5;
+        // Don't do this - int can only hold numbers of a certain size.
+        //int reallyBigNum = 100000000000000000000000000000;
+
+        string quoteOfTheDay = "Perfect is the enemy of good.";
+        Console.WriteLine("Here is a quote for you: " + quoteOfTheDay);
+
+        // Declaration - create the variable, without assigning a value.
+        string message;
+        // Initialization - putting the 1st value into the variable.
+        message = $"Hope you've enjoyted this weird app, {name}!";
+        Console.WriteLine(message);
+
+        Console.WriteLine("");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+    }
+}
