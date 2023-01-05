d9ae5cdaa7eae2c6c9c74b0a83936790617f8d5e
Charlie Tekulve <charlietekulve@gmail.com>
2023-01-05 01:51:21 -0500

    update

--- ListsDemo/ListsDemo/Program.cs
+++ ListsDemo/ListsDemo/Program.cs
@@ -10,22 +10,28 @@
         // Array, you use .length but with a list, you use .count
         Console.WriteLine($"You currently have {inventory.Count}X items in your inventory.");
         
-        inventory.Add("A medallion");
-        inventory.Add("A fireproof cloak");
-        inventory.Add("A protective talisman");
-        inventory.Insert(1,"A bag of dried mushrooms");
+        inventory.Add("medallion");
+        inventory.Add("fireproof cloak");
+        inventory.Add("protective talisman");
+        inventory.Insert(1,"bag of dried mushrooms");
 
-        for (int i = 0; i < inventory.Count; i++)
-        {
-            Console.WriteLine($"> {inventory[i]}");
-        }
+        Console.WriteLine($"You currently have {inventory.Count}X items in your inventory.");
+        Console.WriteLine($"Is a medallion currently in the list? {inventory.Contains("Medallion")}");
         
-        // foreach (string item in inventory)
+        // for (int i = 0; i < inventory.Count; i++)
         // {
-        //     Console.WriteLine(inventory);
+        //     Console.WriteLine($"> {inventory[i]}");
         // }
         
-        Console.WriteLine($"You currently have {inventory.Count}X items in your inventory.");
+        // inventory.ForEach(delegate(string s)
+        // {
+        //     Console.WriteLine($">>>{s}");
+        // });
+        //
+        foreach (string item in inventory)
+        {
+            Console.WriteLine($"> {item}");
+        }
         
         Console.WriteLine("\nPress any key to exit...");
         Console.ReadKey();
